7993917df48f1de4d7bb83446127c82fa33cf65a
ilhamibnu <e41210178@student.polije.ac.id>
2023-05-05 17:14:24 +0700

    fix navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,8 +1,10 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex justify-content-center">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-            <a class="navbar-brand brand-logo" href="/user"><img src={{ asset('images/logo-tuansilat.svg') }} alt="logo" /></a>
-            <a class="navbar-brand brand-logo-mini" href="/user"><img src={{ asset('images/logo-tuansilat-mini.svg') }} alt="logo" /></a>
+            <a class="navbar-brand brand-logo" href="/user"><img src={{ asset('images/logo-tuansilat.svg') }}
+                    alt="logo" /></a>
+            <a class="navbar-brand brand-logo-mini" href="/user"><img src={{ asset('images/logo-tuansilat-mini.svg') }}
+                    alt="logo" /></a>
             <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                 <span class="mdi mdi-sort-variant"></span>
             </button>
@@ -22,5 +24,9 @@
                 </div>
             </li>
         </ul>
+        <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button"
+            data-toggle="offcanvas">
+            <span class="mdi mdi-menu"></span>
+        </button>
     </div>
 </nav>
